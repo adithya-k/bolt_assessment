238e8db6296bf33ce353b5270a5eee0a22d379d5
Adithya K <adithyak.1rn09cs003@gmail.com>
2025-09-12 15:46:01 +0530

    Update airplane_models_json.sql

--- sql/create_tables/airplane_models_json.sql
+++ sql/create_tables/airplane_models_json.sql
@@ -1,7 +1,7 @@
 -- RAW: store original JSON for airplane models
 CREATE TABLE IF NOT EXISTS raw.airplane_models_json (
   raw_json STRING,
-  ingest_ts TIMESTAMP DEFAULT current_timestamp()
+  ingestion_ts TIMESTAMP DEFAULT current_timestamp()
 )
 USING delta
 LOCATION 's3://bucket-location/raw/airplane_models_json/';
